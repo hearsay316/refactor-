0b6e2a273156f052f28c0a4be411a00028d17843
hearsay <hearsay316@users.noreply.github.com>
2022-01-05 13:42:34 +0800

    1 编译、测试、提交，然后使用内联变量（123）手法内   联play变量

--- 重构第一张第一个小结案例.js
+++ 重构第一张第一个小结案例.js
@@ -31,7 +31,7 @@ function statement (invoice, plays) {
         { style: "currency", currency: "USD",
             minimumFractionDigits: 2 }).format;
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(perf, play)
 
 // add volume credits
@@ -73,3 +73,9 @@ function amountFor(aPerformance, play) {
     }
     return result;
 }
+function playFor(aPerformance){
+    return plays[aPerformance.playID];
+}
+// function playFor(aPerformance) {
+//     return plays[aPerformance.playID];
+// }

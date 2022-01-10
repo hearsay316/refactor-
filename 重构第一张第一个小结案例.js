f0bcc156531049e56fd272711b0c5fa68a0b2947
hearsay <hearsay316@users.noreply.github.com>
2022-01-10 13:52:23 +0800

    把与更新volumeCredits变量相关的代码都集中到一起， 有利于以查询取代临时变量（178）手法的施展。第一步同 样是先对变量的计算过程应用提炼函数（106）手法。

--- 重构第一张第一个小结案例.js
+++ 重构第一张第一个小结案例.js
@@ -25,15 +25,17 @@ let invoices = [
 ]
 function statement (invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
     // const format = new Intl.NumberFormat("en-US",
     //     { style: "currency", currency: "USD",
     //         minimumFractionDigits: 2 }).format;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
         result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
+    }
+    let volumeCredits = 0;
+    for (let perf  of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
     }
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
